9d35a2f5704be9294851660bc587d490606de0b5
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2024-06-14 00:43:59 -0300

    Login and Register

---
+++ src/composables/UseAuthUser.js
@@ -0,0 +1,64 @@
+import { ref } from 'vue'
+import useSupabase from 'src/boot/supabase'
+
+const data = ref(null)
+
+export default function useAuthUser() {
+    const supabase = useSupabase()
+
+    const login = async ({ email, password }) => {
+        const { data, error } = await supabase.auth.signIn({ email, password })
+        if (error) throw error
+        return data
+    }
+
+    const loginWithSocialProvider = async (provider) => {
+        const { data, error } = await supabase.auth.signIn({ provider })
+        if (error) throw error
+        return data
+    }
+
+    const logout = async () => {
+        const { error } = await supabase.auth.signOut()
+        if (error) throw error
+    }
+
+    const isLoggedIn = () => {
+        return !!userData.value
+    }
+
+    const register = async ({ email, password, ...meta }) => {
+        const { data, error } = await supabase.auth.signUp(
+            { email, password },
+            {
+                data: meta,
+                redirectTo: `${window.location.origin}/me?fromEmail=registrationConfirmation`
+            }
+        )
+        if (error) throw error
+        return data
+    }
+
+    const updateProfile = async (profileData) => {
+        const { data, error } = await supabase.auth.update(profileData)
+        if (error) throw error
+        return data
+    }
+
+    const sendPasswordResetEmail = async (email) => {
+        const { data, error } = await supabase.auth.api.resetPasswordForEmail(email)
+        if (error) throw error
+        return data
+    }
+
+    return {
+        data,
+        login,
+        loginWithSocialProvider,
+        logout,
+        isLoggedIn,
+        register,
+        updateProfile,
+        sendPasswordResetEmail
+    }
+}
---
+++ src/pages/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+    <q-form class="row justify-center" @submit.prevent="handleLogin">
+      <p class="col-12 text-h5 text-center">Registrar</p>
+      <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
+        <q-input label="Nome" />
+        <q-input label="Email" />
+        <q-input label="Password" />
+        <div class="full-width q-pt-md">
+          <q-btn label="Register" color="primary" class="full-width" outlined rounded type="submit" />
+        </div>
+      </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: 'PageRegister',
+
+
+})
+</script>
