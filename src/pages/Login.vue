f677abca62efbdc44ebd71f8df9464ccab145457
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2024-06-18 21:36:22 -0300

    Login Input Password Type

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,7 +4,7 @@
       <p class="col-12 text-h5 text-center">Login</p>
       <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
         <q-input label="Email" v-model="form.email" />
-        <q-input label="Password" v-model="form.password" />
+        <q-input label="Password" v-model="form.password" type="password" />
         <div class="full-width q-pt-md">
           <q-btn label="Login" color="primary" class="full-width" outlined rounded type="submit" />
         </div>
